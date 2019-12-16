fc129ce9ff296c050817faa4496d9b94ab8cddc7
Leevanni <leevannigoncalves@gmail.com>
2019-12-16 11:54:43 -0500

    fewfe

--- src/main/java/SprintFlowApiApplication.java
+++ src/main/java/SprintFlowApiApplication.java
@@ -2,8 +2,9 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-
+@ComponentScan
 @SpringBootApplication
 public class SprintFlowApiApplication {
 
